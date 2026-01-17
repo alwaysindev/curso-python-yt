9feabfa3347ffd64e721c107498031e19842443a
Alejandro Cárabe <113419791+Alexcar934@users.noreply.github.com>
2026-01-17 14:14:26 +0100

    subidas_modificaciones_directo

---
+++ 01_Basics/prueba_pep.py
@@ -0,0 +1,16 @@
+x = 1
+y = 2
+z = 3 + 4
+
+mensaje = "Hola"
+otro_mensaje = "Mundo"
+
+resultado_suma = x + y + z
+
+print("Valor de x:", x)
+print("Valor de y:", y)
+print("Valor de z:", z)
+print("Mensaje combinado:", mensaje + " " + otro_mensaje)
+print("Resultado suma:", resultado_suma)
+print("Comparación x<y:", x < y)
+print("Comparación z>y:", z > y)
